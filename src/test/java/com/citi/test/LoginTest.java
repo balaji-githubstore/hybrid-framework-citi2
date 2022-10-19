60c64d8e4f7f92bba15234f99a965c2b6cbce73f
Administrator <Administrator@172.20.0.152>
2022-10-19 17:26:25 +0530

    login test with browser config

---
+++ src/test/java/com/citi/test/LoginTest.java
@@ -0,0 +1,62 @@
+package com.citi.test;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class LoginTest {
+	WebDriver driver;
+	
+	@BeforeMethod
+	public void setup()
+	{
+		WebDriverManager.chromedriver().setup();
+
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+
+		driver.get("https://opensource-demo.orangehrmlive.com/");
+	}
+	
+	@AfterMethod
+	public void teardown()
+	{
+		driver.quit();
+	}
+	
+	@Test
+	public void invalidCredentialTest()
+	{
+		driver.findElement(By.name("username")).sendKeys("john");
+		driver.findElement(By.cssSelector("[name='password']")).sendKeys("john123");
+		driver.findElement(By.xpath("//button[@type='submit']")).click();
+		
+		String actualError=driver.findElement(By.xpath("//div[contains(@class,'alert-content')]")).getText();
+		Assert.assertEquals(actualError, "Invalid credentials");
+	}
+
+	@Test
+	public void validCredentialTest() {
+		driver.findElement(By.name("username")).sendKeys("Admin");
+		driver.findElement(By.cssSelector("[name='password']")).sendKeys("admin123");
+		driver.findElement(By.xpath("//button[@type='submit']")).click();
+
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
+		wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//span[text()='Admin']")));
+
+		String actualText = driver.findElement(By.xpath("//h5")).getText();
+		Assert.assertEquals(actualText, "Employee Information");
+	}
+
+}
