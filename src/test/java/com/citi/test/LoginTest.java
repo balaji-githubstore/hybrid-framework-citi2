b647bde6d21d7546ba01dc7818ec49383d77955d
Administrator <Administrator@172.20.0.152>
2022-10-20 15:42:05 +0530

    dataprovider added in utilities

--- src/test/java/com/citi/test/LoginTest.java
+++ src/test/java/com/citi/test/LoginTest.java
@@ -3,46 +3,23 @@ package com.citi.test;
 import java.time.Duration;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import com.citi.base.AutomationWrapper;
-
-import io.github.bonigarcia.wdm.WebDriverManager;
-
+import com.citi.utilities.DataUtils;
+	
 public class LoginTest extends AutomationWrapper {
-	
-	@DataProvider
-	public Object[][] invalidCredentialData()
-	{
-		Object[][] main=new Object[2][3];
-		
-		main[0][0]="john";
-		main[0][1]="john123";
-		main[0][2]="Invalid credentials";
-		
-		main[1][0]="peter";
-		main[1][1]="peter123";
-		main[1][2]="Invalid credentials";
-		
-		return main;
-	}
-	
-	@Test(dataProvider = "invalidCredentialData")
-	public void invalidCredentialTest(String username,String password,String expectedError)
-	{
+
+	@Test(dataProvider = "invalidCredentialData",dataProviderClass = DataUtils.class)
+	public void invalidCredentialTest(String username, String password, String expectedError) {
 		driver.findElement(By.name("username")).sendKeys(username);
 		driver.findElement(By.cssSelector("[name='password']")).sendKeys(password);
 		driver.findElement(By.xpath("//button[@type='submit']")).click();
-		
-		String actualError=driver.findElement(By.xpath("//div[contains(@class,'alert-content')]")).getText();
+
+		String actualError = driver.findElement(By.xpath("//div[contains(@class,'alert-content')]")).getText();
 		Assert.assertEquals(actualError, expectedError);
 	}
 
---
+++ src/test/java/com/citi/utilities/DataUtils.java
@@ -0,0 +1,24 @@
+package com.citi.utilities;
+
+import org.testng.annotations.DataProvider;
+
+public class DataUtils {
+	
+	@DataProvider
+	public Object[][] invalidCredentialData()
+	{
+		Object[][] main=new Object[2][3];
+		
+		main[0][0]="john";
+		main[0][1]="john123";
+		main[0][2]="Invalid credentials";
+		
+		main[1][0]="peter";
+		main[1][1]="peter123";
+		main[1][2]="Invalid credentials";
+		
+		return main;
+	}
+	
+
+}
