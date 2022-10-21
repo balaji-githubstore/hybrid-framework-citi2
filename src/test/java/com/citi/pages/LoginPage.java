d1eeb6ea72d58da5f1d5e6d1edf3feb1e287f4ce
Administrator <Administrator@172.20.0.152>
2022-10-21 15:49:40 +0530

    no page object

--- src/test/java/com/citi/pages/LoginPage.java
+++ src/test/java/com/citi/pages/LoginPage.java
@@ -6,23 +6,5 @@ import org.openqa.selenium.WebDriver;
 public class LoginPage {
 
 	
-	public static void enterUsername(WebDriver driver,String username)
-	{
-		driver.findElement(By.name("username")).sendKeys(username);
-	}
 	
-	public static void enterPassword(WebDriver driver,String password)
-	{
-		driver.findElement(By.cssSelector("[name='password']")).sendKeys(password);
-	}
-	
-	public static void clickOnLogin(WebDriver driver)
-	{
-		driver.findElement(By.xpath("//button[@type='submit']")).click();
-	}
-	
-	public static String getInvalidErrorMessage(WebDriver driver)
-	{
-		return driver.findElement(By.xpath("//div[contains(@class,'alert-content')]")).getText();
-	}
 }
